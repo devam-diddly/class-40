e75479283c5f8af6b6d494e1567f41eccd8df4cc
devam-diddly <73830888+devam-diddly@users.noreply.github.com>
2021-02-06 10:45:54 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,54 @@
+class Form{
+    constructor(){
+        this.input = createInput("name");
+        this.reset = createButton("reset");
+        this.button = createButton("play");
+        this.greeting = createElement('h3');
+    }
+
+     hide(){
+         this.greeting.hide();
+         this.button.hide();
+         this.input.hide();
+     }
+
+    display(){
+        var title = createElement('h2');
+        title.html("car racing game ");
+        title.position(displayWidth/2-50,0);
+
+        this.input.position(displayWidth/2-40,displayHeight/2-80);
+       
+        this.button.position(displayWidth/2+30,displayHeight/2-80); 
+
+        this.reset.position(displayWidth-100,20);
+
+        this.button.mousePressed(() => 
+        { 
+
+            this.input.hide();
+            this.button.hide(); 
+
+            player.name = this.input.value();
+            
+            playerCount += 1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+
+            this.greeting.html("welcome to car racing game ... lets get started or die " + player.name );
+            this.greeting.position(displayWidth/2-70,displayHeight/4);
+            
+        });
+
+        this.reset.mousePressed(() =>  
+        {
+
+            player.updateCount(0);
+            game.update(0);
+
+        });
+
+    }
+
+}
